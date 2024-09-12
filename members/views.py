083ef44cd139bb287965692dcfe8971f77fec94c
Vitor Berlink Santos <vitor.santos@bagov.ba.gov.br>
2024-09-12 10:48:09 -0300

    projeto inicial

---
+++ members/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Tela de login
+def login():
+
+    return
+
+# Tela de cadastro
+def cadastro():
+
+    return
+
+# Tela principal do usuário
+def main_page_user():
+
+    return
